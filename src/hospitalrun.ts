fdaae41e0ab431b4187b7d4a6442d3cc52be1e76
Michael J Feher <github@phearzero.com>
2019-09-08 15:38:18 -0500

    :sparkles: (cli) Lazy Loading CLI wrapper
    Adding basic command line interface `hospitalrun` which looks for any `@hospitalrun` packages that
    export commands

---
+++ src/hospitalrun.ts
@@ -0,0 +1,62 @@
+#!/usr/bin/env node
+
+const pkg = require('../package.json');
+const program = require('commander');
+const spawn = require('cross-spawn');
+program.version(pkg.version);
+
+
+/**
+ * Finds Local NPM Packages
+ */
+const findLocalPackages = () => spawn.sync('cross-env', [
+    'npm_config_loglevel=silent',
+    'npm', 'list', '--json', '--depth', '0'
+]);
+/**
+ * Finds Global NPM Packages
+ */
+const findGlobalPackages = () => spawn.sync('cross-env', [
+    'npm_config_loglevel=silent',
+    'npm', 'list', '--json', '-g', '--depth', '0'
+]);
+
+// Get the Packages
+const localPkgs = JSON.parse(findLocalPackages().stdout);
+const globalPkgs = JSON.parse(findGlobalPackages().stdout);
+
+// Combine the results
+const pkgList = {...localPkgs.dependencies, ...globalPkgs.dependencies};
+
+// Example Package Export to be included in the CLI
+const pkgTest = {
+    commands: {
+        start: {
+            name: 'hospitalrun-frontend-start',
+            description: 'Start the frontend'
+        }
+    }
+};
+
+// Lazy Load any commands that are available
+// NOTE: We can hardcode the commands to make the cli faster or present a spinner while it fetches dependencies
+Object.keys(pkgList)
+    .filter((pkg: any) => pkg.match('@hospitalrun'))
+    .forEach((pkg: string) => {
+        let mod = require(pkg)
+        if (typeof mod.commands !== 'undefined') {
+            Object.keys(mod.commands).forEach((cmd: any) => {
+                program.command(
+                    cmd,
+                    mod.commands[cmd].description,
+                    {executableFile: mod.commands[cmd].name}
+                )
+            })
+        }
+    });
+
+// Run the Program
+program.parse(process.argv)
+
+// Export for testing
+export {pkg, program}
