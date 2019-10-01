464b9ad2c2c8df10a3dccddbbccaf242c679496e
Matteo Vivona <kapa89@me.com>
2019-10-01 14:51:52 +0200

    refactor(cli): updates semantic release and deps
    closes #3

--- src/hospitalrun.ts
+++ src/hospitalrun.ts
@@ -1,68 +1,77 @@
 #!/usr/bin/env node
 
-const pkg = require('../package.json');
-const program = require('commander');
-const spawn = require('cross-spawn');
+const pkg = require("../package.json");
+const program = require("commander");
+const spawn = require("cross-spawn");
 program.version(pkg.version);
-
 
 /**
  * Finds Local NPM Packages
  */
-const findLocalPackages = () => spawn.sync('cross-env', [
-    'npm_config_loglevel=silent',
-    'npm', 'list', '--json', '--depth', '0'
-]);
+const findLocalPackages = () =>
+  spawn.sync("cross-env", [
+    "npm_config_loglevel=silent",
+    "npm",
+    "list",
+    "--json",
+    "--depth",
+    "0"
+  ]);
 /**
  * Finds Global NPM Packages
  */
-const findGlobalPackages = () => spawn.sync('cross-env', [
-    'npm_config_loglevel=silent',
-    'npm', 'list', '--json', '-g', '--depth', '0'
-]);
+const findGlobalPackages = () =>
+  spawn.sync("cross-env", [
+    "npm_config_loglevel=silent",
+    "npm",
+    "list",
+    "--json",
+    "-g",
+    "--depth",
+    "0"
+  ]);
 
 // Get the Packages
 const localPkgs = JSON.parse(findLocalPackages().stdout);
 const globalPkgs = JSON.parse(findGlobalPackages().stdout);
 
 // Combine the results
-const pkgList = {...localPkgs.dependencies, ...globalPkgs.dependencies};
+const pkgList = { ...localPkgs.dependencies, ...globalPkgs.dependencies };
 
 // Example Package Export to be included in the CLI
 const pkgTest = {
-    commands: {
-        start: {
-            name: 'hospitalrun-frontend-start',
-            description: 'Start the frontend'
-        }
+  commands: {
+    start: {
+      name: "frontend-start",
+      description: "Start the frontend"
     }
+  }
 };
 
 // Lazy Load any commands that are available
 // NOTE: We can hardcode the commands to make the cli faster or present a spinner while it fetches dependencies
 Object.keys(pkgList)
-    .filter((pkg: any) => pkg.match('@hospitalrun') && !pkg.match('@hospitalrun/cli'))
-    .forEach((pkg: string) => {
-        try{
-            let mod = require(pkg)
+  .filter(
+    (pkg: any) => pkg.match("@hospitalrun") && !pkg.match("@hospitalrun/cli")
+  )
+  .forEach((pkg: string) => {
+    try {
+      let mod = require(pkg);
 
-            if (typeof mod.commands !== 'undefined') {
-                Object.keys(mod.commands).forEach((cmd: any) => {
-                    program.command(
-                        cmd,
-                        mod.commands[cmd].description,
-                        {executableFile: mod.commands[cmd].name}
-                    )
-                })
-            }
-        } catch(err){
-            console.log(`${err} loading ${pkg}`)
-        }
-
-    });
+      if (typeof mod.commands !== "undefined") {
+        Object.keys(mod.commands).forEach((cmd: any) => {
+          program.command(cmd, mod.commands[cmd].description, {
+            executableFile: mod.commands[cmd].name
+          });
+        });
+      }
+    } catch (err) {
+      console.log(`${err} loading ${pkg}`);
+    }
+  });
 
 // Run the Program
-program.parse(process.argv)
+program.parse(process.argv);
 
 // Export for testing
-export {pkg, program}
+export { pkg, program };
